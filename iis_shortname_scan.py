7aca38e88ce9a4041ca233c10e24dcda1340d125
wuerror <995576393@qq.com>
2022-12-07 20:15:41 +0800

    解决SSL: CERTIFICATE_VERIFY_FAILED

--- iis_shortname_scan.py
+++ iis_shortname_scan.py
@@ -5,6 +5,16 @@
 import sys
 import threading
 import time
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 try:
     # python 2.x
